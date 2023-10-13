a571ea7b461d948d722c265ba22ba70aa63599ea
adityaFutGit <137064471+adityaFutGit@users.noreply.github.com>
2023-10-14 00:12:12 +0530

    Delete lib directory

--- lib/Widgets/customColors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/animation.dart';
-
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color iconColor = Color.fromARGB(255, 157, 178, 206);
-  static const Color darkgrey = Color.fromARGB(255, 82, 82, 82);
-}
